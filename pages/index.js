d789b8f189387d74e9b72e8c0528e5d899d7b874
addrway <addrway@outlook.com>
2026-02-19 16:20:51 -0500

    Create index.js
    ind

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "system-ui", padding: 24, maxWidth: 900, margin: "0 auto" }}>
+      <h1>Addrway App</h1>
+      <p>Next.js is working. Next step: address validation.</p>
+    </main>
+  );
+}
